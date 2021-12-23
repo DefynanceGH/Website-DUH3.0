4af4c95c7287f8b680b08aeb31c6bca0ec09dbf1
Camilo Morales <camiloandres.1509@gmail.com>
2021-12-23 18:12:42 -0500

    Updating website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "style.css"
+import "tailwindcss/base.css"
+import "demos/AgencyLandingPage"
+import AnimationRevealPage from "helpers/AnimationRevealPage"
+import Hero from "components/hero/FullWidthWithImage"
+import AgencyLandingPage from "demos/AgencyLandingPage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AgencyLandingPage />
+  )
 }
 
 export default App;
